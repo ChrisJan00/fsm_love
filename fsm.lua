daffdc819f7dfd98e88f02f4eb5ff52ba78d79d6
Christiaan Janssen <cjan_ynzn@yahoo.es>
2015-06-15 15:10:59 +0200

    testing

--- fsm.lua
+++ fsm.lua
@@ -1,4 +1,26 @@
-FSM = FSM or {}
+-- fsm_love: a small library for finite state machines
+
+-- zlib license
+-- Copyright (c) 2014-2015 Christiaan Janssen
+
+-- This software is provided 'as-is', without any express or implied
+-- warranty. In no event will the authors be held liable for any damages
+-- arising from the use of this software.
+
+-- Permission is granted to anyone to use this software for any purpose,
+-- including commercial applications, and to alter it and redistribute it
+-- freely, subject to the following restrictions:
+
+-- 1. The origin of this software must not be misrepresented; you must not
+--    claim that you wrote the original software. If you use this software
+--    in a product, an acknowledgement in the product documentation would be
+--    appreciated but is not required.
+-- 2. Altered source versions must be plainly marked as such, and must not be
+--    misrepresented as being the original software.
+-- 3. This notice may not be removed or altered from any source distribution.
+
+
+local FSM = {}
 
 local function increaseExponential(dt, var, amount)
     if var < 1 then
@@ -21,6 +43,16 @@ local function decreaseExponential(dt, var, amount)
 end
 
 local state_proto = {
+    withInit = function(self, func)
+        self.init = func
+        return self
+    end,
+
+    withFinish = function(self, func)
+        self.finish = func
+        return self
+    end,
+
     withUpdate = function(self, func) 
         self.update = func
         return self
@@ -31,7 +63,7 @@ local state_proto = {
         return self
     end,
     copy = function(self)
-        local newState = FSM.makeState(self.name)
+        local newState = FSM.createState(self.name)
         for k,v in pairs(self) do
             newState[k] = v
         end
@@ -94,7 +126,7 @@ local state_mt = {
     __index = function(table, key) return state_proto[key] end,
 }
 
-function FSM.makeState(stateName)
+function FSM.createState(stateName)
     local state = { name = stateName }
     setmetatable(state, state_mt)
     return state
@@ -111,6 +143,13 @@ local machine_proto = {
             self.stateList[state.name] = state
         else
             table.insert(self.stateList, state)
+        end
+        return self
+    end,
+
+    addStates = function(self,...)
+        for i=1,select("#",...) do
+            self:addState(select(i,...))
         end
         return self
     end,
@@ -140,6 +179,9 @@ local machine_proto = {
             self.current:finish()
         end
         self.current = newState
+        if self.current and self.current.init then
+            self.current:init()
+        end
         if self.current and self.current.begin then
             self.current:begin()
         end
@@ -154,6 +196,7 @@ local machine_proto = {
         newMachine.stateList = {}
         for k,v in pairs(self.stateList) do
             newMachine.stateList[k] = v:copy()
+            newMachine.stateList[k].parent = newMachine
         end
         return newMachine
     end,
@@ -176,9 +219,10 @@ local machine_mt = {
     __index = function(table, key) return machine_proto[key] end,
 }
 
-function FSM.create()
+function FSM.createMachine()
     local machine = { stateList = {} }
     setmetatable(machine, machine_mt)
     return machine
 end
 
+return FSM
---
+++ fsm_tests.lua
@@ -0,0 +1,139 @@
+local fsm = require 'fsm'
+
+local get_filename = function() 
+    local fn = debug.getinfo(2,"S").short_src
+    local index = string.find(fn, "/[^/]*$")
+    if index then
+        return fn:sub(index+1)
+    end
+    return fn
+end
+
+local filename = get_filename()
+
+local function compare(result, expected)
+    if result == expected then return end
+    if type(result) == "number" and type(expected) == "number" and 
+        result == expected then
+        return true
+    end
+
+    local linenumber = debug.getinfo(2,"l").currentline
+    print("FAIL")
+    print(filename..":"..linenumber)
+    print("result:\n"..tostring(result))
+    print("expected:\n"..tostring(expected))
+    error()
+end
+
+function runTests(tests)
+    local passedCount = 0
+    for i=1,#tests do
+        io.write("....test "..i.." ..")
+        local passed,errmsg = pcall(tests[i])
+        if passed then
+            print("OK")
+            passedCount = passedCount + 1
+        elseif errmsg then
+                print("FAIL")
+                print(errmsg)
+        end
+    end
+
+    print("\nPassed "..passedCount.." / "..#tests.." tests")
+end
+
+local tests = {
+    function()
+        -- create empty machine, update once, draw once -> should not crash
+        local testMac = fsm.createMachine()
+        testMac:update(1)
+        testMac:draw()
+    end,
+
+    function()
+        -- create machine, add 1 state, update without activation (nothing should happen), update with activation (something should happen)
+        local testVal = 0
+        local testMac = fsm.createMachine():addState(fsm.createState():withDraw(function() testVal = testVal + 1 end))
+        testMac:update(1)
+        testMac:draw()
+        compare(testVal, 0)
+        testMac:activateState(1)
+        testMac:update(1)
+        testMac:draw()
+        compare(testVal, 1)
+    end,
+
+    function()
+        -- create machine with 2 tests, switch between them on timer
+        local currentLabel = "zero"
+        local state1 = fsm.createState("first"):withInit(function() currentLabel = "one" end):timedSwitch("second",1)
+        local state2 = fsm.createState("second"):withInit(function() currentLabel = "two" end):timedSwitch("first",1)
+        local testMac = fsm.createMachine():addStates(state1, state2)
+        compare(currentLabel, "zero")
+        testMac:setInitialState("first"):restart()
+        compare(currentLabel,"one")
+        testMac:update(1)
+        compare(currentLabel,"two")
+        testMac:update(1)
+        compare(currentLabel,"one")
+    end,
+
+    function()
+        -- create machine with 2 tests, switch with expo
+        local currentLabel = "zero"
+        local state1 = fsm.createState("first"):withInit(function() currentLabel = "one" end):expoUpSwitch("second",0.01)
+        local state2 = fsm.createState("second"):withInit(function() currentLabel = "two" end):expoDownSwitch("first",0.01)
+        local testMac = fsm.createMachine():addStates(state1, state2)
+        compare(currentLabel, "zero")
+        testMac:setInitialState("first"):restart()
+        compare(currentLabel,"one")
+        testMac:update(1)
+        compare(currentLabel,"two")
+        testMac:update(1)
+        compare(currentLabel,"one")
+    end,
+
+    function()
+        -- create machine with 2 tests, switch with condition
+        local currentLabel = "zero"
+        local state1 = fsm.createState("first"):withInit(function() currentLabel = "one" end):conditionSwitch("second",function() return true end)
+        local state2 = fsm.createState("second"):withInit(function() currentLabel = "two" end):conditionSwitch("first",function() return false end)
+        local testMac = fsm.createMachine():addStates(state1, state2)
+        compare(currentLabel, "zero")
+        testMac:setInitialState("first"):restart()
+        compare(currentLabel,"one")
+        testMac:update(1)
+        compare(currentLabel,"two")
+        testMac:update(1)
+        compare(currentLabel,"two")
+        return true
+    end,
+
+    function()
+        -- create machine with 1 state.  clone machine.  update both.  each machine should be independent from the other one
+        local state1 = fsm.createState("a"):timedSwitch("b",2)
+        local state2 = fsm.createState("b")
+        state1.count = 0
+        local mac1 = fsm.createMachine():addStates(state1, state2)
+        local mac2 = mac1:copy()
+
+        mac1:activateState("a")
+        mac2:activateState("a")
+
+        mac1:update(1)
+        mac2:update(1)
+
+        compare(mac1.current.name, "a")
+        compare(mac2.current.name, "a")
+
+        mac1:update(0.5)
+        mac2:update(1)
+        compare(mac1.current.name, "a")
+        compare(mac2.current.name, "b")
+
+    end,
+
+}
+
+runTests(tests)
